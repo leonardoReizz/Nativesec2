e9efb33d0aa6614e73c3a2b0e8bdd6c37c41d032
Leonardo Reis <amarantee.reis@gmail.com>
2023-01-13 15:46:02 -0300

    work

--- src/main/components/user-config/use-cases/update-user/index.ts
+++ src/main/components/user-config/use-cases/update-user/index.ts
@@ -1,10 +1,13 @@
+import { KeyRepositoryAPI } from 'main/components/keys/repositories/key-repository-api';
 import { UserConfigRepositoryDatabase } from '../../repositories/user-config-repository-database';
 import { UpdateUserController } from './update-user-config-controller';
 import { UpdateUserConfigUseCase } from './update-user-config-use-case';
 
 const userConfigRepositoryDatabase = new UserConfigRepositoryDatabase();
+const keyRepositoryAPI = new KeyRepositoryAPI();
 const updateUserConfigUseCase = new UpdateUserConfigUseCase(
-  userConfigRepositoryDatabase
+  userConfigRepositoryDatabase,
+  keyRepositoryAPI
 );
 const updateUserConfigController = new UpdateUserController(
   updateUserConfigUseCase
